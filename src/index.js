24c0dcd223f698e6a226ef044474a37569c14a6b
Eugeshakw <eugeshakw@gmail.com>
2024-06-11 13:21:16 +0300

    start

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react-template'>
+    <BrowserRouter basename='/test-task'>
     <App />
     </BrowserRouter>
     
